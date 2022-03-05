a819a8682f46b7ba70d657d80409a9f509706ad0
Rik Verbeek <rvrbk.dev@gmail.com>
2022-03-05 09:04:47 +0100

    change

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">AnOther Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
